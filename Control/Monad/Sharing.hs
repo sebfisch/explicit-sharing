56523cd179a2464a6e07846508a331b0f06e110f
Sebastian Fischer <sebf@informatik.uni-kiel.de>
2009-05-27 19:43:33 +0200

    simplified module structure, generalized type classes, and added documentation.

--- Control/Monad/Sharing.hs
+++ Control/Monad/Sharing.hs
@@ -1,23 +1,122 @@
-{-# LANGUAGE
-     MultiParamTypeClasses,
-     Rank2Types
+{-# LANGUAGE MultiParamTypeClasses, 
+             FlexibleInstances,
+             Rank2Types 
   #-}
 
-module Control.Monad.Sharing where
+-- | Module      : Control.Monad.Sharing
+-- | Copyright   : Sebastian Fischer
+-- | License     : PublicDomain
+-- |
+-- | Maintainer  : Sebastian Fischer (sebf@informatik.uni-kiel.de)
+-- | Stability   : experimental
+-- |
+-- | This library provides an interface to monads that support explicit
+-- | sharing.
+module Control.Monad.Sharing (
+
+  module Control.Monad,
+
+  -- * Classes
+
+  Sharing(..), Trans(..),
+
+  -- $predefined
+
+  -- * Evaluation
+
+  eval,
+
+  -- * Monadic lists
+
+  List(..), nil, cons
+
+ ) where
 
 import Control.Monad
 
-class MonadPlus m => Nondet m a
+-- | Interface of monads that support explicit sharing.
+class Sharing m
  where
-  mapNondet :: (forall b . Nondet m b => m b -> m (m b)) -> a -> m a
+  -- | Yields an action that returns the same results as the given
+  -- | action but whose effects are only executed once. Especially,
+  -- | when the resulting action is duplicated it returns the same
+  -- | result at every occurrence.
+  share :: Trans m a b => m a -> m (m b)
 
-eval :: Nondet m a => a -> m a
-eval = mapNondet (\a -> a >>= eval >>= return . return)
-
-class MonadPlus m => Sharing m
+-- | Interface to transform nested monadic data types. The provided
+-- | function @trans@ is supposed to map the given function on every
+-- | monadic argument. The result of @trans@ may be of the same type
+-- | as the argument but can also be of a different type, e.g. to
+-- | convert a value with nested monadic arguments to a corresponding
+-- | value without.
+class Trans m a b
  where
-  share :: Nondet m a => m a -> m (m a)
+  trans :: (forall c d . Trans m c d => m c -> m (m d)) -> a -> m b
 
-shareRec :: (Sharing m, Nondet m a) => (m a -> m a) -> m (m a)
-shareRec f = let x = share (f (x >>= id)) in x
+-- | Lifts all monadic effects in nested monadic values to the top
+-- | level. If @m@ is a monad for non-determinism and the argument a
+-- | data structure with nested non-determinism then the result
+-- | corresponds to the normal form of the argument.
+eval :: (Monad m, Trans m a b) => a -> m b
+eval = trans (\a -> a >>= eval >>= return . return)
+
+-- $predefined 
+--
+-- We provide instances of the @Trans@ class for some predefined
+-- Haskell types. For flat types the function @trans@ just returns its
+-- argument which has no arguments to which the given function could
+-- be applied.
+
+instance Monad m => Trans m Bool Bool
+ where
+  trans _ = return
+
+instance Monad m => Trans m Int Int
+ where
+  trans _ = return
+
+instance Monad m => Trans m Char Char
+ where
+  trans _ = return
+
+instance Monad m => Trans m Float Float
+ where
+  trans _ = return
+
+instance Monad m => Trans m Double Double
+ where
+  trans _ = return
+
+-- | An instance for lists with monadic elements.
+instance (Monad m, Trans m a a) => Trans m [m a] [m a]
+ where
+  trans f = mapM f
+
+-- | An instance for lists with monadic elements that lifts all
+-- | monadic effects to the top level and yields a list with
+-- | non-monadic elements.
+instance (Monad m, Trans m a a) => Trans m [m a] [a]
+ where
+  trans f = mapM (join . f)
+
+-- | Data type for lists where both the head and tail are monadic.
+data List m a = Nil | Cons (m a) (m (List m a))
+
+-- | The empty monadic list.
+nil :: Monad m => m (List m a)
+nil = return Nil
+
+-- | Constructs a non-empty monadic list.
+cons :: Monad m => m a -> m (List m a) -> m (List m a)
+cons x xs = return (Cons x xs)
+
+instance (Monad m, Trans m a a) => Trans m (List m a) (List m a)
+ where
+  trans _ Nil         = return Nil
+  trans f (Cons x xs) = return Cons `ap` f x `ap` f xs
+
+instance (Monad m, Trans m a a) => Trans m (List m a) [a]
+ where
+  trans _ Nil         = return []
+  trans f (Cons x xs) = return (:) `ap` join (f x) `ap` join (f xs)
 
--- Control/Monad/Sharing/Lazy.hs
+++ Control/Monad/Sharing/Lazy.hs
@@ -1,18 +1,115 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExistentialQuantification, 
+             MultiParamTypeClasses,
+             FlexibleContexts,
+             Rank2Types 
+  #-}
 
+-- | Module      : Control.Monad.Sharing.Lazy
+-- | Copyright   : Sebastian Fischer
+-- | License     : PublicDomain
+-- |
+-- | Maintainer  : Sebastian Fischer (sebf@informatik.uni-kiel.de)
+-- | Stability   : experimental
+-- |
+-- | Implements explicit sharing by passing a heap using a state monad
+-- | implemented by a combination of a continuation- with a reader
+-- | monad. The definitions are inlined and hand-optimized to increase
+-- | performance.
 module Control.Monad.Sharing.Lazy (
 
-  Lazy, runLazy, evalLazy,
+  module Control.Monad.Sharing,
 
-  module Control.Monad,
-  module Control.Monad.Sharing
+  Lazy, evalLazy
 
  ) where
 
-import Control.Monad
+import Control.Monad.State
 import Control.Monad.Sharing
-import Control.Monad.Sharing.Lazy.ContReaderNoThunksInlined
 
-evalLazy :: Monad m => Nondet (Lazy m) a => Lazy m a -> m a
+-- For fast and easy implementation of typed stores..
+import Unsafe.Coerce
+
+import qualified Data.IntMap as M
+
+-- | Continuation-based, store-passing implementation of explicit
+-- | sharing. It is an inlined version of @ContT (ReaderT Store m)@
+-- | where the result type of continuations is polymorphic.
+newtype Lazy m a = Lazy {
+
+  -- | Runs a computation of type @Lazy m a@ with given continuation
+  -- | and store.
+  fromLazy :: forall w . (a -> Store -> m w) -> Store -> m w
+ }
+
+-- | Lifts all monadic effects to the top-level and unwraps the monad
+-- | transformer for explicit sharing.
+evalLazy :: (Monad m, Trans (Lazy m) a b) => Lazy m a -> m b
 evalLazy m = runLazy (m >>= eval)
+
+-- private declarations
+
+runLazy :: Monad m => Lazy m a -> m a
+runLazy m = fromLazy m (\a _ -> return a) (Store 1 M.empty)
+
+-- Stores consist of a fresh-reference counter and a heap represented
+-- as IntMap.
+data Store = Store Int (M.IntMap Untyped)
+
+-- The monad instance is an inlined version of the instances for
+-- continuation and reader monads.
+instance Monad m => Monad (Lazy m)
+ where
+  return x = Lazy (\c -> c x)
+  a >>= k  = Lazy (\c s -> fromLazy a (\x -> fromLazy (k x) c) s)
+  fail err = Lazy (\_ _ -> fail err)
+
+-- The @MonadPlus@ instance reuses corresponding operations of the
+-- base monad.
+instance MonadPlus m => MonadPlus (Lazy m)
+ where
+  mzero       = Lazy (\_ _ -> mzero)
+  a `mplus` b = Lazy (\c s -> fromLazy a c s `mplus` fromLazy b c s)
+
+-- With continuations, a reader monad can be made a state monad.
+instance Monad m => MonadState Store (Lazy m)
+ where
+  get   = Lazy (\c s -> c s s)
+  put s = Lazy (\c _ -> c () s)
+
+-- The @Sharing@ instance memoizes nested monadic values recursively.
+instance Monad m => Sharing (Lazy m)
+ where
+  share a = memo (a >>= trans share)
+
+withTypeOf :: a -> a -> a
+withTypeOf = const
+
+-- This is an inlined version of the following definition:
+-- 
+-- > memo :: MonadState Store m => m a -> m (m a)
+-- > memo a = do key <- getFreshKey
+-- >             return $ do thunk <- lookupHNF key
+-- >                         case thunk of
+-- >                           Just x  -> return x
+-- >                           Nothing -> do x <- a
+-- >                                         insertHNF key x
+-- >                                         return x
+--
+memo :: Lazy m a -> Lazy m (Lazy m a)
+memo a = Lazy (\c (Store key heap) ->
+      c (Lazy (\c s@(Store _ heap) -> 
+         case M.lookup key heap of
+          Just x  -> c (typed x) s
+          Nothing -> fromLazy a
+           (\x (Store other heap) -> 
+              c x (Store other (M.insert key (Untyped x) heap))) s))
+        (Store (succ key) heap))
+
+-- Easy and fast hack to store typed data. An implementation using
+-- Data.Typeable is possible but clutters the code with additional
+-- class constraints.
+data Untyped = forall a . Untyped a
+
+typed :: Untyped -> a
+typed (Untyped x) = unsafeCoerce x
 
