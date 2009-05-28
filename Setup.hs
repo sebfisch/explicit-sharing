4e8bdfbc2988983076e84039d4adab96bbae1d3b
Sebastian Fischer <sebf@informatik.uni-kiel.de>
2009-05-28 18:08:54 +0200

    hide transformers package for cabal test

--- Setup.hs
+++ Setup.hs
@@ -5,6 +5,7 @@ import Distribution.Simple
 main = defaultMainWithHooks $ simpleUserHooks { runTests = runTestSuite }
 
 runTestSuite _ _ _ _ =
- do pid <- runCommand "ghc -hide-package monads-fd -e main Test.hs"
+ do pid <- runCommand $ "ghc -hide-package monads-fd "
+                     ++ "-hide-package transformers -e main Test.hs"
     waitForProcess pid >>= exitWith
 
